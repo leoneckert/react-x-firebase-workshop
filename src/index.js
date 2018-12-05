d20c54e524bbafd462bf45b34bc762357b98435f
leoneckert <leoneckert@mailbox.org>
2018-12-05 12:58:47 +0800

    finished start setup?

--- src/index.js
+++ src/index.js
@@ -4,12 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
 ReactDOM.render(
-  <div>
-    <div className="intro"><p>React x Firebase Workshop</p></div>
-    <App />
-    <div className="intro"><p>thanks for playing, Leon</p></div>
-  </div>, document.getElementById('root'));
+  <Router>
+    <div>
+      <div className="intro"><p>React x Firebase Workshop</p></div>
+      <Route exact path="/" component={()=>
+            <div>
+              <App />
+            </div>
+        } />
+      <Route exact path="/solved" component={()=>
+            <div>
+              <p>HELLO</p>
+            </div>
+        } />
+      <div className="intro"><p>thanks for playing, Leon</p></div>
+    </div>
+  </Router>
+  
+  , 
+  
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
