b0de4bcfab7e851c613a2dcf79d7010f0bbabdee
leoneckert <leoneckert@mailbox.org>
2018-12-05 21:36:09 +0800

    width of explantion box

--- src/Other/ExplanationBox.js
+++ src/Other/ExplanationBox.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 class ExplanationBox extends Component {
   render() { 
-      return <div className="explanation"><p>{this.props.t}</p></div>
+      return(
+        <div className="explanationWrapper">
+        <div className="explanation"><p>{this.props.t}</p></div>
+        </div>
+      )
     ;
   }
 }
